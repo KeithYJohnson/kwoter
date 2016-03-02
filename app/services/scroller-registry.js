a494731b9292ea3a89cb12ec008486becbbfa76f
Keith Johnson <KeithYJohnson@gmail.com>
2016-03-02 17:44:30 -0500

    creating (skeleton)  downward scroll listening and adding it to the registry

--- app/services/scroller-registry.js
+++ app/services/scroller-registry.js
@@ -2,14 +2,19 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   rightScrollListener: Ember.inject.service(),
+  downwardScrollListener: Ember.inject.service(),
 
   registerScrollListeners(){
     let rightScrollListener = this.get('rightScrollListener');
+    let downwardScrollListener = this.get('downwardScrollListener');
     rightScrollListener.turnOn();
+    downwardScrollListener.turnOn();
   },
 
   unregisterScrollListeners(){
     let rightScrollListener = this.get('rightScrollListener');
+    let downwardScrollListener = this.get('downwardScrollListener');
     rightScrollListener.turnOff();
+    downwardScrollListener.turnOff();
   }
 });
---
+++ tests/unit/services/downward-scroll-listener-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('service:downward-scroll-listener', 'Unit | Service | downward scroll listener', {
+  // Specify the other units that are required for this test.
+  // needs: ['service:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  var service = this.subject();
+  assert.ok(service);
+});
