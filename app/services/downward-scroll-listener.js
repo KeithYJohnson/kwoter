11a14e57cc14eebe65717867dc89b49a91659c0f
Keith Johnson <KeithYJohnson@gmail.com>
2016-03-02 17:53:45 -0500

    isScrolledToBottom()

--- app/services/downward-scroll-listener.js
+++ app/services/downward-scroll-listener.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 import ScrollerSwitch from 'kwoter/mixins/scroller-switch';
 
+const buffer = 50;
+
 export default Ember.Service.extend(ScrollerSwitch, {
   didScroll(){
-  }
+    if ( this.isScrolledToBottom() ){
+
+    }
+  },
+
+  isScrolledToBottom(){
+    let $viewportHeight = Ember.$(window).height();
+    let $documentHeight = Ember.$(document).height();
+
+    let $bottomOfViewport = Ember.$(window).scrollTop();
+    let $breakpoint = $documentHeight - $viewportHeight;
+
+    return ($bottomOfViewport + buffer) > $breakpoint;
+  },
 });
